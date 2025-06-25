68237efb91cbc3df7ac890ad3d5934b8950a455c
KarenHerreraDev <karenherrera.dev@gmail.com>
2025-06-24 21:30:36 -0300

    cambios en codigo

--- src/controller/proveedores.controller.js
+++ src/controller/proveedores.controller.js
@@ -2,26 +2,21 @@ import db from '../config/database.js';
 
 export const postProveedor = async (req, res) => {
 
-    //obtener los datos del body
     const { NombreProveedor, TelefonoProveedor, EmailProveedor,DomicilioProveedor } = req.body;
 
-    //validamos que los datos no esten vacios
     if (!NombreProveedor || !TelefonoProveedor || !EmailProveedor || !DomicilioProveedor) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios' });
     }
 
     try {
-        //insertamos el proveedor en la base de datos
         const queryInsert = 'INSERT INTO proveedores (NombreProveedor, TelefonoProveedor, EmailProveedor, DomicilioProveedor) VALUES (?, ?, ?, ?)';
 
-        //ejecutamos la consulta para insertar el proveedor
         db.query(queryInsert, [NombreProveedor, TelefonoProveedor, EmailProveedor, DomicilioProveedor], (errorInsert, resultsInsert) => {
             if (errorInsert) {
                 console.error('Error al insertar el proveedor:', errorInsert);
                 return res.status(500).json({ message: 'Error al insertar el proveedor' });
             }
 
-            //si la insercion fue exitosa devolvemos el proveedor insertado
             res.status(201).json({
                 message: 'Proveedor creado exitosamente',
             });
@@ -34,17 +29,14 @@ export const postProveedor = async (req, res) => {
 
 export const obtenerTodosLosProveedores = async (req, res) => {
     try {
-        //consulta para obtener todos los proveedores
         const querySelect = 'SELECT * FROM proveedores';
 
-        //ejecutamos la consulta para obtener los proveedores
         db.query(querySelect, (errorSelect, resultsSelect) => {
             if (errorSelect) {
                 console.error('Error al obtener los proveedores:', errorSelect);
                 return res.status(500).json({ message: 'Error al obtener los proveedores' });
             }
 
-            //si la consulta fue exitosa devolvemos los proveedores
             res.status(200).json(resultsSelect);
         });
 
@@ -57,22 +49,19 @@ export const obtenerProveedorPorId = async (req, res) => {
     const { idProveedores } = req.params;
 
     try {
-        //consulta para obtener un proveedor por su id
         const querySelect = 'SELECT * FROM proveedores WHERE idProveedores = ?';
 
-        //ejecutamos la consulta para obtener el proveedor
         db.query(querySelect, [idProveedores], (errorSelect, resultsSelect) => {
             if (errorSelect) {
                 console.error('Error al obtener el proveedor:', errorSelect);
                 return res.status(500).json({ message: 'Error al obtener el proveedor' });
             }
 
-            //si no se encuentra el proveedor devolvemos un error
+
             if (resultsSelect.length === 0) {
                 return res.status(404).json({ message: 'Proveedor no encontrado' });
             }
 
-            //si la consulta fue exitosa devolvemos el proveedor
             res.status(200).json(resultsSelect[0]);
         });
 
@@ -85,22 +74,18 @@ export const obtenerProveedorPorTel = async (req, res) => {
     const { TelefonoProveedor } = req.params;
 
     try {
-        //consulta para obtener un proveedor por su telefono
         const querySelect = 'SELECT * FROM proveedores WHERE TelefonoProveedor = ?';
 
-        //ejecutamos la consulta para obtener el proveedor
         db.query(querySelect, [TelefonoProveedor], (errorSelect, resultsSelect) => {
             if (errorSelect) {
                 console.error('Error al obtener el proveedor:', errorSelect);
                 return res.status(500).json({ message: 'Error al obtener el proveedor' });
             }
 
-            //si no se encuentra el proveedor devolvemos un error
             if (resultsSelect.length === 0) {
                 return res.status(404).json({ message: 'Proveedor no encontrado' });
             }
 
-            //si la consulta fue exitosa devolvemos el proveedor
             res.status(200).json(resultsSelect[0]);
         });
 
@@ -112,19 +97,15 @@ export const obtenerProveedorPorTel = async (req, res) => {
 export const obtenerProveedorPorEmail = async (req, res) => {
     const { EmailProveedor } = req.params;
     try {
-        //consulta para obtener un proveedor por su email
         const querySelect = 'SELECT * FROM proveedores WHERE EmailProveedor = ?';
-        //ejecutamos la consulta para obtener el proveedor
         db.query(querySelect, [EmailProveedor], (errorSelect, resultsSelect) => {
             if (errorSelect) {
                 console.error('Error al obtener el proveedor:', errorSelect);
                 return res.status(500).json({ message: 'Error al obtener el proveedor' });
             }
-            //si no se encuentra el proveedor devolvemos un error
             if (resultsSelect.length === 0) {
                 return res.status(404).json({ message: 'Proveedor no encontrado' });
             }   
-            //si la consulta fue exitosa devolvemos el proveedor
             res.status(200).json(resultsSelect[0]);
         });
     } catch (error) {
@@ -135,19 +116,15 @@ export const obtenerProveedorPorEmail = async (req, res) => {
 export const obtenerProveedorPorNombre = async (req, res) => {
     const { NombreProveedor } = req.params;
     try {
-        //consulta para obtener un proveedor por su nombre
         const querySelect = 'SELECT * FROM proveedores WHERE NombreProveedor = ?';
-        //ejecutamos la consulta para obtener el proveedor
         db.query(querySelect, [NombreProveedor], (errorSelect, resultsSelect) => {
             if (errorSelect) {
                 console.error('Error al obtener el proveedor:', errorSelect);
                 return res.status(500).json({ message: 'Error al obtener el proveedor' });
             }
-            //si no se encuentra el proveedor devolvemos un error
             if (resultsSelect.length === 0) {
                 return res.status(404).json({ message: 'Proveedor no encontrado' });
             }   
-            //si la consulta fue exitosa devolvemos el proveedor
             res.status(200).json(resultsSelect[0]);
         });
     } catch (error) {
@@ -159,16 +136,13 @@ export const actualizarProveedor = async (req, res) => {
     const { idProveedores } = req.params;
     const { NombreProveedor, TelefonoProveedor, EmailProveedor, DomicilioProveedor } = req.body;
 
-    //validamos que los datos no esten vacios
     if (!NombreProveedor || !TelefonoProveedor || !EmailProveedor || !DomicilioProveedor) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios' });
     }
 
     try {
-        //consulta para actualizar un proveedor por su id
         const queryUpdate = 'UPDATE proveedores SET NombreProveedor = ?, TelefonoProveedor = ?, EmailProveedor = ?, DomicilioProveedor = ? WHERE idProveedores = ?';
 
-        //ejecutamos la consulta para actualizar el proveedor
         db.query(queryUpdate, [NombreProveedor, TelefonoProveedor, EmailProveedor, DomicilioProveedor, idProveedores], (errorUpdate, resultsUpdate) => {
             
             if (errorUpdate) {
@@ -179,7 +153,6 @@ export const actualizarProveedor = async (req, res) => {
                 return res.status(404).json({ message: 'Proveedor no encontrado' });
             }
             
-            //si la actualizacion fue exitosa devolvemos el proveedor actualizado
             res.status(200).json({
                 message: 'Proveedor actualizado exitosamente',
             });
@@ -194,17 +167,14 @@ export const eliminarProveedor = async (req, res) => {
     const { idProveedores } = req.params;
 
     try {
-        //consulta para eliminar un proveedor por su id
         const queryDelete = 'DELETE FROM proveedores WHERE idProveedores = ?';
 
-        //ejecutamos la consulta para eliminar el proveedor
         db.query(queryDelete, [idProveedores], (errorDelete, resultsDelete) => {
             if (errorDelete) {
                 console.error('Error al eliminar el proveedor:', errorDelete);
                 return res.status(500).json({ message: 'Error al eliminar el proveedor' });
             }
 
-            //si la eliminacion fue exitosa devolvemos un mensaje de exito
             res.status(200).json({
                 message: 'Proveedor eliminado exitosamente',
             });
